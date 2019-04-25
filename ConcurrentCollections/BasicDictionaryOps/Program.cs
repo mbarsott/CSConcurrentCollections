786bde92752242fde04f00424b790a60a084903c
Marcelo Barsotti <mbarsott@gmail.com>
2019-04-24 21:02:37 -0400

    2. Introducing ConcurrentDictionary

---
+++ ConcurrentCollections/BasicDictionaryOps/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace BasicDictionaryOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            IDictionary<string, int> stock = new ConcurrentDictionary<string, int>();
+            var stock = new ConcurrentDictionary<string, int>();
+            stock.TryAdd("jDays", 4);
+            stock.TryAdd("technologyhour", 3);
+
+            Console.WriteLine($"No. of shirts in stock = {stock.Count}");
+
+            bool success = stock.TryAdd("pluralsight", 6);
+            Console.WriteLine($"Add succeeded? {success}");
+            success = stock.TryAdd("pluralsight", 6);
+            Console.WriteLine($"Add succeeded? {success}");
+
+            stock["buddhistgeeks"] = 5;
+
+            //            stock["pluralsight"] = 7; // up from 6, we just bought one
+            //            success = stock.TryUpdate("pluralsight", 7, 6);
+            //            Console.WriteLine($"\r\npluralsight = {stock["pluralsight"]}, did update work? {success}");
+            int psStock = stock.AddOrUpdate("pluralsight", 1, (key, oldvalue) => oldvalue + 1);
+            Console.WriteLine("New value is " + psStock);
+
+            Console.WriteLine($"stock[pluralsight] = {stock.GetOrAdd("pluralsight", 0)}");
+
+            //            stock.Remove("jDays");
+            success = stock.TryRemove("jDays", out int jDaysValue);
+            if (success)
+            {
+                Console.WriteLine($"Value removed was: {jDaysValue}");
+            }
+
+            Console.WriteLine("\r\nEnumerating:");
+            foreach (var keyValPair in stock)
+            {
+                Console.WriteLine($"{keyValPair.Key}: {keyValPair.Value}");
+            }
+        }
+    }
+}
--- ConcurrentCollections/SubmitOrders/Program.cs
+++ ConcurrentCollections/SubmitOrders/Program.cs
@@ -12,27 +12,30 @@ namespace SubmitOrders
         private static void Main(string[] args)
         {
             var orders = new Queue<string>();
-//            var orders = new ConcurrentQueue<string>();
-//            var task1 = Task.Run(()=>PlaceOrders(orders, "Mark"));
-//            var task2 = Task.Run(()=> PlaceOrders(orders, "Ramdevi"));
-//            Task.WaitAll(task1, task2);
+            //            var orders = new ConcurrentQueue<string>();
+            //            var task1 = Task.Run(()=>PlaceOrders(orders, "Mark"));
+            //            var task2 = Task.Run(()=> PlaceOrders(orders, "Ramdevi"));
+            //            Task.WaitAll(task1, task2);
             PlaceOrders(orders, "Mark");
             PlaceOrders(orders, "Ramdevi");
-//            foreach (var order in orders) ProcessOrder(order);
+            //            foreach (var order in orders) ProcessOrder(order);
             Parallel.ForEach(orders, ProcessOrder);
-            foreach (var order in orders) Console.WriteLine($"ORDER: {order}");
+            foreach (var order in orders)
+            {
+                Console.WriteLine($"ORDER: {order}");
+            }
         }
 
         private static void ProcessOrder(string order)
         {
         }
 
-//        private static void PlaceOrders(ConcurrentQueue<string> orders, string customerName)
+        //        private static void PlaceOrders(ConcurrentQueue<string> orders, string customerName)
         private static void PlaceOrders(Queue<string> orders, string customerName)
         {
             for (var i = 0; i < 5; i++)
             {
-                Thread.Sleep(1);
+                Thread.Sleep(10);
                 var orderName = string.Format($"{customerName} wants t-shirt {i + 1}");
                 lock (_lockObj)
                 {
