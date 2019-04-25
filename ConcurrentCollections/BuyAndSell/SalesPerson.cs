9a366d828ba20f24945374dcd25df2491c157389
Marcelo Barsotti <mbarsott@gmail.com>
2019-04-24 22:09:23 -0400

    3. Concurrent Dictionary Demo

---
+++ ConcurrentCollections/BuyAndSell/SalesPerson.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace BuyAndSell
+{
+    public class SalesPerson
+    {
+        public string Name { get; private set; }
+
+        public SalesPerson(string name)
+        {
+            this.Name = name;
+        }
+
+        public void Work(StockController controller, TimeSpan workDay)
+        {
+            Random rand = new Random(Name.GetHashCode());
+            DateTime start = DateTime.Now;
+            while (DateTime.Now - start < workDay)
+            {
+                //                Thread.Sleep(rand.Next(100));
+                bool buy = (rand.Next(6) == 0);
+                string itemName = Program.AllShirtNames[rand.Next(Program.AllShirtNames.Count)];
+                if (buy)
+                {
+                    int quantity = rand.Next(9) + 1;
+                    controller.BuyStock(itemName, quantity);
+                    //                    DisplayPurchase(itemName, quantity);
+                }
+                else
+                {
+                    bool success = controller.trySellItem(itemName);
+                    //                    DisplaySaleAttempt(success, itemName);
+                }
+            }
+
+            Console.WriteLine($"SalesPerson {Name} signing off");
+        }
+
+        private void DisplaySaleAttempt(bool success, string itemName)
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            if (success)
+            {
+                Console.WriteLine($"Thread {threadId}: {Name} sold {itemName}");
+            }
+            else
+            {
+                Console.WriteLine($"Thread {threadId}: {Name}: Out of stock of {itemName}");
+            }
+        }
+
+        private void DisplayPurchase(string itemName, int quantity)
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: {Name} bought {quantity} of {itemName}");
+        }
+    }
+}
---
+++ ConcurrentCollections/BuyAndSell/StockController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+
+namespace BuyAndSell
+{
+    public class StockController
+    {
+        ConcurrentDictionary<string, int> _stock = new ConcurrentDictionary<string, int>();
+        private int _totalQuantityBought;
+        private int _totalQuantitySold;
+
+        public void BuyStock(string item, int quantity)
+        {
+            _stock.AddOrUpdate(item, quantity, (key, oldValue) => oldValue + quantity);
+            Interlocked.Add(ref _totalQuantityBought, quantity);
+        }
+
+        public void DisplayStatus()
+        {
+            int totalStock = _stock.Values.Sum();
+            Console.WriteLine("\r\nBought = " + _totalQuantityBought);
+            Console.WriteLine("Sold   = " + _totalQuantitySold);
+            Console.WriteLine("Stock   = " + totalStock);
+            int error = totalStock + _totalQuantitySold - _totalQuantityBought;
+            if (error == 0)
+            {
+                Console.WriteLine("Stock levels match");
+            }
+            else
+            {
+                Console.WriteLine("Error in stock level: " + error);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Stock levels by item: ");
+            foreach (var itemName in Program.AllShirtNames)
+            {
+                int stockLevel = _stock.GetOrAdd(itemName, 0);
+                Console.WriteLine($"{itemName,-30}: {stockLevel}");
+            }
+        }
+
+        public bool trySellItem(string item)
+        {
+            bool success = false;
+            int newStockLevel = _stock.AddOrUpdate(item,
+                (itemName) =>
+                {
+                    success = false;
+                    return 0;
+                },
+                (itemName, oldValue) =>
+                {
+                    if (oldValue == 0)
+                    {
+                        success = false;
+                        return 0;
+                    }
+                    else
+                    {
+                        success = true;
+                        return oldValue - 1;
+                    }
+                });
+            if (success)
+            {
+                Interlocked.Increment(ref _totalQuantitySold);
+            }
+
+            return success;
+        }
+
+        public bool TrySellItem2(string item)
+        {
+            int newStockLevel = _stock.AddOrUpdate(item, -1, (key, oldValue) => oldValue - 1);
+            if (newStockLevel < 0)
+            {
+                _stock.AddOrUpdate(item, 1, (key, oldvalue) => oldvalue + 1);
+                return false;
+            }
+            else
+            {
+                Interlocked.Increment(ref _totalQuantitySold);
+                return true;
+            }
+        }
+    }
+}
